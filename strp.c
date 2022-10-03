1c9d9776b1ad9da2bdd7c4793747a07498a0873f
Josh <jschoenberg@princeton.edu>
2022-10-03 14:57:19 -0400

    Str_copy in strp try 1

---
+++ strp.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stddef.h>
+#include "str.h"
+
+size_t Str_getLength(const char *pcSrc)
+{
+   const char *pcEnd;
+   assert(pcSrc != NULL);
+   pcEnd = pcSrc;
+   while (*pcEnd != '\0')
+      pcEnd++;
+   return (size_t)(pcEnd - pcSrc);
+}
+
+char *Str_copy(char *pcDest, const char *pcSrc) { 
+    assert(pcDest != NULL);
+    assert(pcSrc != NULL);
+    char *pcDestStart = pcDest;
+    while (*pcSrc != '0')  {
+        *pcDest = *pcSrc;
+        pcDest++;
+        pcSrc++;
+    }
+    *pcDest = '\0';
+return pcDestStart;
+}
+
+char *Str_concat(char *pcDest, const char *pcSrc) {
+}
+
+int Str_compare(const char *pc1, const char *pc2) {
+
+}
+
+
+
+
+char *Str_search(const char *haystack, const char *needle) {
+
+}
