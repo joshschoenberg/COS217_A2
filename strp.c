5d5135d83f8c8725c6794c3c6683b7b8390511e3
Josh <jschoenberg@princeton.edu>
2022-10-03 15:03:47 -0400

    try 8

--- strp.c
+++ strp.c
@@ -45,3 +45,38 @@ char *Str_search(const char *haystack, const char *needle) {
     return ptr;
 }
 */ 
+char *Str_search(const char haystack[], const char needle[]) {
+    size_t haystackIndex;
+    size_t needleIndex;
+    assert(haystack != NULL);
+    assert(needle != NULL);
+    haystackIndex = 0;
+    needleIndex = 0;
+    /* Return the haystack if the needle is empty */ 
+    if (needle[needleIndex] == '\0')
+        return (char *) haystack;
+    while (haystack[haystackIndex] != '\0') {
+         /* If needle appears in haystack, return pointer to first 
+        character of first occurrence of needle in haystack */
+        if (needle[needleIndex] == '\0') {
+            return (char *) (haystack + haystackIndex - Str_getLength(needle));
+        }
+        /* If the next character matches, increment the needle and 
+        haystack indices */
+        else if (haystack[haystackIndex] == needle[needleIndex]) {
+                needleIndex++;
+                haystackIndex++;
+        }
+        /* If haystack character is the first letter of needle, go to 
+        the second character of each */
+        else if (haystack[haystackIndex] == *needle) {
+            haystackIndex++;
+            needleIndex += 1; /* Needle index becomes the second one */
+        }
+        else {
+            needleIndex = 0;
+            haystackIndex++;
+        }
+    }
+    return NULL;
+}
